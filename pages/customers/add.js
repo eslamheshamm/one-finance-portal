5c9f6eded3f2d09073ff168de48ebf43e6a068e6
eslamheshamm <esl2mhesham@gmail.com>
2022-10-31 14:26:08 +0200

    push

--- pages/customers/add.js
+++ pages/customers/add.js
@@ -1,12 +1,11 @@
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
-
+// import { useSession } from "next-auth/react";
+// import { useRouter } from "next/router";
 import DashboardLayout from "../../components/Dashboard/Layout";
 import AddCustomerForm from "../../components/Forms/AddCustomer";
 
 const AddCustomerPage = () => {
-	const { data: session, status } = useSession();
-	const router = useRouter();
+	// const { data: session, status } = useSession();
+	// const router = useRouter();
 
 	return (
 		<DashboardLayout>
